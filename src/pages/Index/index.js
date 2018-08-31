fccdd5e96ae5f1b864cf8e26524b5fb85fcc15b5
qiuqiu <qiuqiu@xintiaotime.com>
2018-08-31 17:25:08 +0800

    Electron + React-Router

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Button} from 'antd'
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router, Route, Switch, Link,
+  HashRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
+import { METHODS } from 'http';
 import styles from './index.scss';
 import MenuIndex from './menu';
 
@@ -17,8 +19,41 @@ class Index extends React.Component {
         <h1 className={styles.title}>Hello World</h1>
         <Link to="/">主页</Link>
         <Link to="menu">副页</Link>
+        <Link to="counter">计数器</Link>
       </React.Fragment>
     );
+  }
+}
+class Counter extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    
+  }
+  add = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  sub = () => {
+    this.setState({
+      count: this.state.count  -1
+    })
+  }
+  render(){
+    return(
+      <React.Fragment>
+        <h1>计数器</h1>
+        <h2>{this.state.count}</h2>
+        <Link to="/">主页</Link>
+        <Link to="menu">副页</Link>
+        <Link to="counter">计数器</Link>
+        <Button onClick={this.add}>+</Button>
+        <Button onClick={this.sub}>-</Button>
+      </React.Fragment>
+    )
   }
 }
 class App extends React.Component {
@@ -28,6 +63,7 @@ class App extends React.Component {
         <Switch>
         <Route path='/' exact component={Index} />
         <Route path='/menu' component={MenuIndex}/>
+        <Route path='/counter' component={Counter}/>
         </Switch>
 
       </Router>
--- src/pages/Index/menu.js
+++ src/pages/Index/menu.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Menu } from 'antd';
+import {
+  Link,
+} from 'react-router-dom';
 
 class MenuIndex extends React.Component {
   render() {
     return (
-      <Menu theme="dark">
+      <div>
+        <Link to="/">主页</Link>
+        <Link to="menu">副页</Link>
+        <Link to="counter">计数器</Link>
+        <Menu theme="dark">
         <Menu.Item>
           选项一
         </Menu.Item>
@@ -19,6 +26,8 @@ class MenuIndex extends React.Component {
         </Menu.Item>
 
       </Menu>
+      </div>
+
     );
   }
 }
